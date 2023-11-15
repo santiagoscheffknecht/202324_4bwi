86fa9a493b9cd05683f4e6d1c82ee1ed7bed029a
santiago <santiago.scheffknecht@student.htldornbirn.at>
2023-11-15 12:02:02 +0100

    addedAPI/workedDesign

--- HTML_Ajax/Ue1/script.js
+++ HTML_Ajax/Ue1/script.js
@@ -1,11 +1,17 @@
 const loadData = () => {
     fetch("https://api.openligadb.de/getbltable/%C3%B6bl1/2023").then((result) => {
         result.json().then((data) => {
+            console.log(data);
+            data.forEach(club => {
+                console.log(club.teamIconUrl);
+                console.log(club.teamName);
+            });
             fillTable(data);
         })
     })
 }
-    
+
+loadData();
     
     
     
